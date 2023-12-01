f0e8be66b1b08912abcf7a934785167521ac156f
SANTIAGO <vargassantiago97@gmail.com>
2023-12-01 20:04:25 -0300

    x

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -28,12 +28,12 @@ export class InicioComponent {
             { header: 'ORIG', field: 'ok_origen' },
             { header: 'ID', field: 'id' },
             { header: 'Tipo', field: 'tipo' },
-            { header: 'O', field: 'tipo_origen' },
+            { header: 'De', field: 'tipo_origen' },
             { header: 'Fecha', field: 'fecha' },
             { header: 'Cultivo', field: 'cultivo' },
             { header: 'Prod.', field: 'produce' },
             { header: 'Est', field: 'establecimiento' },
-            { header: 'campo', field: 'kg_campo' },
+            { header: 'Campo', field: 'kg_campo' },
             { header: 'Bruto', field: 'kg_bruto' },
             { header: 'Tara', field: 'kg_tara' },
             { header: 'Neto', field: 'kg_neto' },
@@ -81,7 +81,7 @@ export class InicioComponent {
         this.sqlite.getDB('datos_movimientos', this.dbLocal, () => { this.cargados++ })
     }
 
-    ejecutar() {
+    ejecutarCamiones() {
         this.dataTabla = []
 
         this.db["movimientos"].forEach((movimiento: any) => {
@@ -92,6 +92,70 @@ export class InicioComponent {
             const fechaFormato = `${fecha.getFullYear()}/${(fecha.getMonth()+1).toString().padStart(2, '0')}/${(fecha.getDate()).toString().padStart(2, '0')}`
 
             if(fecha < limite){
+                var movimientoLocal:any = this.dbLocal['datos_movimientos'].find((e:any) => { return e.id_movimiento == movimiento.id });
+
+                var dato: any = {
+                    ok_origen: movimientoLocal ? movimientoLocal.ok_origen : '',
+                    ok_destino: movimientoLocal ? movimientoLocal.ok_destino : '',
+                    ok_contrato: movimientoLocal ? movimientoLocal.ok_contrato : '',
+
+                    id: movimiento.id,
+
+                    tipo: movimientoLocal ? movimientoLocal.tipo : '',
+                    fecha: fechaFormato,
+                    cultivo: this.transformar(movimiento.id_grano, 'grano'),
+                    establecimiento: this.transformar(movimiento.id_origen, 'establecimiento'),
+
+                    kg_campo: movimiento.kg_campo,
+                    kg_bruto: movimiento.kg_bruto,
+                    kg_tara: movimiento.kg_tara,
+                    kg_neto: movimiento.kg_neto,
+                    kg_regulacion: movimiento.kg_regulacion,
+                    kg_neto_final_salida: movimiento.kg_neto_final,
+
+                    patente_chasis: this.transformar(movimiento.id_camion, 'patente_chasis'),
+                    patente_acoplado: this.transformar(movimiento.id_camion, 'patente_acoplado'),
+
+                    deposito: this.transformar(movimiento.id_deposito, 'deposito'),
+
+                    kg_acondicionadora: movimientoLocal ? movimientoLocal.kg_acondicionadora : '',
+                    kg_destino: movimientoLocal ? movimientoLocal.kg_destino : '',
+                    kg_mermas: movimientoLocal ? movimientoLocal.kg_mermas : '',
+                    kg_final: movimientoLocal ? movimientoLocal.kg_final : '',
+                    kg_computados: movimientoLocal ? movimientoLocal.kg_computados : '',
+
+                    tipo_origen: movimiento.tipo_origen,
+
+                    produce: this.transformar(movimiento.id_origen, 'produce'),
+                    ctg_origen: this.transformar(movimiento.id, 'nro_ctg'),
+
+                    ctg_destino: movimientoLocal ? movimientoLocal.ctg_destino : '',
+                    retira: movimientoLocal ? movimientoLocal.retira : '',
+                    entrega: movimientoLocal ? movimientoLocal.entrega : '',
+                    acondicionadora: movimientoLocal ? movimientoLocal.acondicionadora : '',
+                    destino: movimientoLocal ? movimientoLocal.destino : '',
+                    contrato: movimientoLocal ? movimientoLocal.contrato : '',
+                    precio: movimientoLocal ? movimientoLocal.precio : '',
+                }
+
+                this.dataTabla.push(dato)
+            }
+        });
+    }
+
+
+    ejecutarOrigen() {
+        this.dataTabla = []
+
+        this.db["movimientos"].forEach((movimiento: any) => {
+
+            //filtros
+            const fecha = new Date(movimiento.fecha)
+            const limiteInf = new Date('2023-07-01')
+            const limiteSup = new Date('2023-07-10')
+            const fechaFormato = `${fecha.getFullYear()}/${(fecha.getMonth()+1).toString().padStart(2, '0')}/${(fecha.getDate()).toString().padStart(2, '0')}`
+
+            if((fecha <= limiteSup) && (fecha >= limiteInf)){
                 var movimientoLocal:any = this.dbLocal['datos_movimientos'].find((e:any) => { return e.id_movimiento == movimiento.id });
 
                 var dato: any = {
